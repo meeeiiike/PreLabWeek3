7768aeed2ae27c81d58640a1b1a2bd5922ace369
meike <g00407095@atu.ie>
2025-09-26 23:08:13 +0100

    added a simple decimal min and size values for name and price

--- src/main/java/ie/atu/week3/Product.java
+++ src/main/java/ie/atu/week3/Product.java
@@ -1,6 +1,8 @@
 package ie.atu.week3;
 
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.hibernate.validator.constraints.NotBlank;
@@ -8,8 +10,8 @@ import org.hibernate.validator.constraints.NotBlank;
 @Data
 @AllArgsConstructor
 public class Product {
-    @NotBlank
+    @Size(min = 1, max = 100)
     private String name;
-    @Positive
+    @Positive @DecimalMin("0.01") @Size(max = 5000)
     private double price;
 }
